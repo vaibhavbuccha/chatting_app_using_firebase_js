3941f312b8d91fe1fa536de6901185bd029fa23f
vaibhav buccha <jainvaibhav415@gmail.com>
2019-11-18 18:11:22 +0530

    Add files via upload

---
+++ firebasechat/index.node.php
@@ -0,0 +1,113 @@
+
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
+
+
+<!-- ---------------include firebase databases-------------------- -->
+<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-database.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-analytics.js"></script>
+
+
+<script>
+ // Your web app's Firebase configuration
+	var firebaseConfig = {
+    apiKey: "AIzaSyC_AhAW9zy8QjBSoS6bPoH0as914AEB91Q",
+    authDomain: "abegya-db.firebaseapp.com",
+    databaseURL: "https://abegya-db.firebaseio.com",
+    projectId: "abegya-db",
+    storageBucket: "abegya-db.appspot.com",
+    messagingSenderId: "791175448065",
+    appId: "1:791175448065:web:792080e5378bbb94ea6200",
+    measurementId: "G-DSM9TG1SSY"
+  };  
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  // create database
+  function insert()
+  {
+  	alert("working");
+	  firebase.database().ref("messages").push().set({
+	  		"senderId":'2',
+	  		"senderName" :'vaibhav',
+	  		"reciverId" :'3',
+	  		"reciverName" :'bipin',
+	  		"message" :'hello'
+	  });
+	}
+
+
+	function getAllDb()
+	{
+		firebase.database().ref('messages').on('child_added',function (snapshot){
+			console.log(snapshot.key);
+		});
+	}
+
+	function getAllDbValue()
+	{
+		firebase.database().ref('messages').on('child_added',function (snapshot){
+			console.log(snapshot.val());
+		});
+	}
+
+	function getCondDbValue()
+	{
+		firebase.database().ref('messages').on('child_added',function (snapshot){
+			// console.log(snapshot.key);
+			if(snapshot.val().senderId == '1' && snapshot.val().reciverId == '4' )
+			{
+				console.log(snapshot.val());
+			}
+			else
+			{
+				console.log('not found');
+			}
+		});
+	}
+
+	function getCondDeleteValue(){
+		firebase.database().ref('messages').on('child_added',function (snapshot){
+
+			if(snapshot.val().senderId == '1' && snapshot.val().reciverId == '4' )
+			{
+				firebase.database().ref("messages").child(snapshot.key).remove();
+			}
+			else
+			{
+				console.log('not found');
+			}
+		});
+	}
+</script>
+
+<button onclick="insert()">create</button>
+<button onclick="getAllDb()">get all</button>
+<button onclick="getAllDbValue()">get all Value</button>
+<button onclick="getCondDbValue()">get conditional Value</button>
+<button onclick="getCondDeleteValue()">get conditional Delete Value</button>
+
+
+<br><br>
+
+<div>
+	<div>
+		
+	</div>
+	<div style=" width: 350px;height: 400px; border:1px solid black;">
+		<div style="width: 350px;height: 400px;  overflow: auto;">
+			
+		</div>
+		<form>
+			<textarea style="width: 280px; height:50px;  ">Enter your message Here!</textarea>
+			<input type="submit" style="float: right;width: 70px;height: 50px;" name="">
+		</form>
+
+	</div>
+</div>
---
+++ firebasechat/index.php
@@ -0,0 +1,90 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
+
+
+<!-- ---------------include firebase databases-------------------- -->
+<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-database.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC_AhAW9zy8QjBSoS6bPoH0as914AEB91Q",
+    authDomain: "abegya-db.firebaseapp.com",
+    databaseURL: "https://abegya-db.firebaseio.com",
+    projectId: "abegya-db",
+    storageBucket: "abegya-db.appspot.com",
+    messagingSenderId: "791175448065",
+    appId: "1:791175448065:web:792080e5378bbb94ea6200",
+    measurementId: "G-DSM9TG1SSY"
+  };                                                                                  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+  // // initialize firebase
+  //  Firebasese.initializeApp(firebaseConfig);
+
+  var myName = prompt("enter your name");
+
+  function sendMessage() {
+    // get message
+    var message = document.getElementById("message").value;
+    // alert(message);
+    // save in database
+    firebase.database().ref("messages").push().set({
+      "sender": myName,
+      "message": message
+    });
+
+    // prevent form from submitting
+    return false;
+  }
+
+  // listen for incomming message
+  firebase.database().ref('messages').on('child_added',function (snapshot){
+    var html = '';
+    //give each message a unique id 
+    html += "<li id='message-"+snapshot.key+"' >"  
+
+    html += '<li>';
+      if(snapshot.val().sender == myName){
+        html += "<button data-id='"+ snapshot.key +"' onclick='deleteMessage(this);' >";
+          html += "Delete";
+        html += "</button>";
+      }
+      html += snapshot.val().sender +": "+snapshot.val().message;
+    html += '</li>';
+
+    document.getElementById('messages').innerHTML += html;
+  })
+
+  function deleteMessage(self)
+  {
+    // get message id for delete message
+    var messageID = self.getAttribute("data-id");
+    // delete the message
+    firebase.database().ref("messages").child(messageId).remove();
+  }
+
+  firebase.database().ref('messages').on("child_removed",function(snapshot){
+    // remove message node
+    document.getElementById("message-"+snapshot.key).innerHTML = "This message has been removed";
+  })
+</script>
+
+<!-- create a form to send message  -->
+
+<form onsubmit="return sendMessage();" >
+  <input type="message" placeholder="Enter message" id="message" autocomplete="off">
+  <input type="submit" >
+</form>
+<!-- create a list -->
+
+<ul id="messages">
+  
+</ul>
